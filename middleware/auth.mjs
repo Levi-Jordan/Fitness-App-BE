f31534793d30fc462c9cb4c41627bea6ea6aab31
Levi-Jordan <Levijordananderson@gmail.com>
2025-06-10 12:10:10 -0600

    More auth stuff

--- middleware/auth.mjs
+++ middleware/auth.mjs
@@ -1,1 +1,12 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default function (req, res, next){
+    let token =req.header("token");
+
+    if (!token) {
+        res.status(401).json({"Auth Denied"})
+    }
+}
--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -2,20 +2,20 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { check, validationResult } from "express-validator";
-import User from "../models/userSchema.mjs"
+import User from "../models/userSchema.mjs";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const router = express.Router();
 
 
 router.post(
-    "/",
+    "/register",
     [
         check('username', 'Name Required').not().isEmpty(),
         check('email', "Need Valid Email").isEmail(),
-        check(
-            'password',
-            "Password Must Be 5 Characters or More"
-        ).isLength({min: 5}),
+        check('password', "Password Must Be 5 Characters or More").isLength({min: 5}),
     ],
     async (req, res) => {
         const errors = validationResult(req);
@@ -24,7 +24,24 @@ router.post(
         }
         const {username, email, password} = req.body;
 
-        
+        try {
+            let user = await User.findOne({ email });
+
+            if (user) {
+                return res.status(400).json({ errors: [{msg: "User Exists"}]});
+            }
+            user = new User({
+                username,
+                email,
+                password,
+            });
+            const salt = await bcrypt.genSalt(10);
+            user.password =await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            const payload =
+        }
     }
 )
 
