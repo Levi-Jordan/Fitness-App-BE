16d6d7628fa4b03e9b8e593b1137682fbdfeb83c
Levi-Jordan <Levijordananderson@gmail.com>
2025-06-10 18:44:39 -0600

    userRoute hopefully done

--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -15,20 +15,20 @@ router.post(
     [
         check('username', 'Name Required').not().isEmpty(),
         check('email', "Need Valid Email").isEmail(),
-        check('password', "Password Must Be 5 Characters or More").isLength({min: 5}),
+        check('password', "Password Must Be 5 Characters or More").isLength({ min: 5 }),
     ],
     async (req, res) => {
         const errors = validationResult(req);
-        if (!errors.isEmpty()){
-            return res.status(400).json({errors: errors.array()});
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
-        const {username, email, password} = req.body;
+        const { username, email, password } = req.body;
 
         try {
             let user = await User.findOne({ email });
 
             if (user) {
-                return res.status(400).json({ errors: [{msg: "User Exists"}]});
+                return res.status(400).json({ errors: [{ msg: "User Exists" }] });
             }
             user = new User({
                 username,
@@ -36,12 +36,26 @@ router.post(
                 password,
             });
             const salt = await bcrypt.genSalt(10);
-            user.password =await bcrypt.hash(password, salt);
+            user.password = await bcrypt.hash(password, salt);
 
             await user.save();
 
-            const payload =
+            const payload = {
+                user: { id: user._id, }
+            }
+            jwt.sign(payload, process.env.jwtSecret, { expiresIn: 3600 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.status(201).json({ token });
+                }
+            )
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ errors: [{ msg: "Server Error" }] })
+
         }
     }
 )
+
+export default router;
 
